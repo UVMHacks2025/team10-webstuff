f479a945ab5d607d2f52d30c5e2bd1343fc3c1f0
ddahal1 <ddahal1@uvm.eud>
2025-03-01 10:32:58 -0500

    pushing code

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
 if __name__ == "__main__":
     app.run(debug=True)

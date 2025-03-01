2e8982b71ad91c82ea50c665d7b296242a381aeb
hsharman <hsharman@uvm.edu>
2025-03-01 15:25:59 -0500

    get info from insert to put into database (broken)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 from psycopg2 import sql
 from connect import auth
@@ -13,15 +13,33 @@ def index():
 def user():
     return render_template('user.html')
 
-@app.route('/insert')
+@app.route('/insert', methods=['GET', 'POST'])
 def insert():
+    if request.method == 'POST':
+        name = request.form['name']
+        quantity = request.form['quantity']
+        expiration_date = request.form['expiration_date']
+        food_type_key = request.form['type']
+        restriction_key = request.form['dietary_restriction']
+        is_perishable = request.form['perishable']
+        location = request.form['location']
+
+        con = auth()
+        cur = con.cursor()
+        cur.execute(
+            'INSERT INTO "INVENTORY" (food_name, quantity, expiration_date, food_type_key, restriction_key, is_perishable, location) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+            (name, quantity, expiration_date, food_type_key, restriction_key, is_perishable, location)
+        )
+        con.commit()
+        con.close()
+        return redirect(url_for('inventory'))
     return render_template('insert.html')
 
 @app.route('/inventory')
 def inventory():
     con = auth()
     cur = con.cursor()
-    cur.execute('SELECT I.container_id, I.quantity, I.entry_date, I.expiration_date, I.food_name FROM "INVENTORY" AS I WHERE I.is_active = 1')
+    cur.execute('SELECT container_id, quantity, entry_date, expiration_date, food_name, food_type_key, restriction_key, is_active, is_perishable, location FROM "INVENTORY" WHERE is_active = 1')
     inventory = cur.fetchall()
     con.close()
     return render_template('inventory.html', inventory=inventory)
--- test.py
+++ test.py
@@ -15,3 +15,5 @@ print(inventory)
 
 con.close()
 
+<body class="bg-blue-100 flex flex-col items-center justify-center min-h-screen text-center p-6"
+      style="background-image: url('static/logo_background.jpg'); background-size: cover; background-position: center; background-repeat: no-repeat; background-attachment: fixed;">
